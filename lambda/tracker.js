9a04e9d58fae4472d8972be53ac3ec04514192e7
Andras Serfozo <subztep@gmail.com>
2021-02-24 19:55:50 +0000

    Insert

--- lambda/tracker.js
+++ lambda/tracker.js
@@ -1,6 +1,25 @@
-exports.handler = async (event, context) => {
+const faunadb = require("faunadb")
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SERVER_SECRET,
+})
+const q = faunadb.query
+
+exports.handler = async event => {
+  const res = client.query(
+    q.Create(q.Collection("Visit"), {
+      data: {
+        ip: event.headers["client-ip"],
+        userAgent: event.headers["user-agent"],
+        created: new Date().toISOString(),
+        account: {
+          connect: event.queryStringParameters.account
+        }
+      }
+    })
+  )
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ event, context }),
+    body: JSON.stringify(res)
   }
 }
