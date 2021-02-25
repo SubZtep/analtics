c18e23498773013aa8a95742ce0e862ce21ec273
Andras Serfozo <subztep@gmail.com>
2021-02-25 05:21:27 +0000

    re

--- lambda/tracker.js
+++ lambda/tracker.js
@@ -1,29 +1,30 @@
-const faunadb = require("faunadb")
-const client = new faunadb.Client({
-  secret: process.env.FAUNADB_SERVER_SECRET,
-})
-const q = faunadb.query
+const fetch = require("node-fetch")
+
+const gql = url => async mutation => {
+  const opts = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ query: mutation }),
+  }
+  const res = await fetch(url, opts)
+  return await res.json()
+}
+
+const createVisit = event => `mutation {
+  createVisit(data: {
+    ip: "${event.headers["client-ip"]}"
+    userAgent: "${event.headers["user-agent"]}",
+    created: "${new Date().toISOString()}",
+    account: {
+      connect: "${event.queryStringParameters.account}"
+    }
+ }) {
+   _id
+ }`
 
 exports.handler = async event => {
   let = statusCode = 200
-  try {
-    client.query(
-      q.Create(q.Collection("Visit"), {
-        data: {
-          ip: event.headers["client-ip"],
-          userAgent: event.headers["user-agent"],
-          created: new Date().toISOString(),
-          account: {
-            connect: event.queryStringParameters.account,
-          },
-        },
-      })
-    )
-  } catch (err) {
-    console.error(err)
-    statusCode = err.requestResult.statusCode
-  }
-
+  await gql("https://graphql.fauna.com/graphql")(createVisit(event))
   return {
     statusCode,
   }
--- scripts/bootstrap-faunadb.js
+++ scripts/bootstrap-faunadb.js
@@ -1,7 +1,33 @@
 const faunadb = require("faunadb")
+const q = faunadb.query
 
 const client = new faunadb.Client({
-  secret: process.env.FAUNADB_SERVER_SECRET
+  secret: "fnAEC2WLDbACAScbEQXB-pJfiXuCPURkI5tUVSiI---"
 })
 
-console.log(client)
+console.log("AAAAA")
+
+const main = async () => {
+  try {
+    const res = await client.query(
+      q.Create(q.Collection("Visit"), {
+        data: {
+          ip: "1.2.3.4",
+          userAgent: "TestAgent",
+          created: new Date().toISOString(),
+          account: {
+            connect: "1291435445219230209"
+          }
+        }
+      })
+    )
+  } catch (err) {
+    // console.log("ERRR", JSON.stringify(err))
+    console.log("ERRR", err.requestResult.statusCode)
+  }
+
+  // console.log(res)
+  process.exit()
+}
+
+main()
