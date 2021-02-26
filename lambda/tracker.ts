7360064c8c3a6b70d961997da30ca868c0448d42
Andras Serfozo <subztep@gmail.com>
2021-02-26 00:49:33 +0000

    Vite plugin + para-param-parse

--- lambda/tracker.ts
+++ lambda/tracker.ts
@@ -1,6 +1,48 @@
 import type { APIGatewayProxyEvent } from "aws-lambda"
 import fetch from "node-fetch"
 
+interface VisitParams {
+  ip?: string
+  userAgent?: String
+  location?: String
+  referrer?: String
+  noscript?: Boolean
+}
+
+const parseVisit = (event: APIGatewayProxyEvent) => {
+  const params: VisitParams = {}
+  if (event.headers["client-ip"]) {
+    params.ip = event.headers["client-ip"]
+  }
+  if (event.headers["user-agent"]) {
+    params.userAgent = event.headers["user-agent"]
+  }
+  if (event.queryStringParameters.referrer) {
+    params.referrer = event.queryStringParameters.noscript
+  } else if (event.headers.referer) {
+    params.referrer = event.headers.referer
+  }
+  if (event.queryStringParameters.location) {
+    params.location = event.queryStringParameters.location
+  }
+  if (event.queryStringParameters.noscript) {
+    params.noscript = event.queryStringParameters.noscript === "true"
+  }
+  return params
+}
+
+const createVisit = (event: APIGatewayProxyEvent) => `mutation {
+  createVisit(data: {
+    ${Object.entries(event).map(([key, value]) => `${key}:${value}`).join(",")},
+    created: "${new Date().toISOString()}",
+    account: {
+      connect: "${event.queryStringParameters.account}"
+    }
+  }) {
+   _id
+  }
+}`
+
 const gql = (url: string, secret: string) => async (mutation: string) =>
   await fetch(url, {
     method: "POST",
@@ -11,20 +53,6 @@ const gql = (url: string, secret: string) => async (mutation: string) =>
     body: JSON.stringify({ query: mutation }),
   })
 
-const createVisit = (event: APIGatewayProxyEvent) => `mutation {
-  createVisit(data: {
-    ip: "${event.headers["client-ip"]}",
-    userAgent: "${event.headers["user-agent"]}",
-    referer: "${event.headers.referer || ""}",
-    noscript: ${event.queryStringParameters.noscript || "false"},
-    created: "${new Date().toISOString()}",
-    account: {
-      connect: "${event.queryStringParameters.account}"
-    }
-  }) {
-   _id
-  }
-}`
 
 exports.handler = async (event: APIGatewayProxyEvent) => {
   const dbres = await gql(process.env.GRAPHQL_URL, process.env.GRAPHQL_SECRET)(createVisit(event))
---
+++ plugins/vite.js
@@ -0,0 +1,34 @@
+/**
+ * @param {{tracker: string, account: string, productionOnly?: boolean}} obj Plugin options
+ */
+const analytics = ({ tracker, account, productionOnly }) => ({
+  name: "analytics",
+  transformIndexHtml(html) {
+    if (productionOnly && process.env.NODE_ENV === "development") {
+      return html
+    }
+
+    const h = `<script async src="${tracker}?account=${account}"></script>`
+    let b =
+      `<noscript><iframe src="${tracker}?account=${account}&noscript=true "` +
+      `height="0" width="0" style="display:none;visibility:hidden"/></noscript>`
+
+    let pos = html.search(/<\/head>/i)
+    if (pos !== -1) {
+      html = html.slice(0, pos) + h + html.slice(pos)
+    } else {
+      b = h + b
+    }
+
+    const body = html.match(/<body.*?>/i)
+    if (body !== null) {
+      pos = body.index + body[0].length
+    } else {
+      pos = html.search(/<\/body>/i)
+      if (pos === -1) {
+        pos = html.length
+      }
+    }
+    return html.slice(0, pos) + b + html.slice(pos)
+  },
+})
