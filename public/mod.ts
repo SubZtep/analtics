32aac700d7affce27c0aa36ecf39694b63fe27b7
Andras Serfozo <subztep@gmail.com>
2021-04-14 07:51:46 +0100

    no hash

--- public/mod.ts
+++ public/mod.ts
@@ -14,7 +14,7 @@ serve({
     intervene: (response) => {
       response.headers.set("Content-Type", "text/html; charset=utf-8");
       response.headers.set("Access-Control-Allow-Origin", "*");
-      response.headers.set("Content-Security-Policy", "default-src * data: 'unsafe-inline' 'unsafe-eval' 'sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=='; img-src * 'self' data:; style-src * 'unsafe-inline' 'unsafe-eval' 'sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=='");
+      response.headers.set("Content-Security-Policy", "default-src * data: 'unsafe-inline' 'unsafe-eval'; img-src * data:; style-src * 'unsafe-inline' 'unsafe-eval'")
       return response;
     },
   }),
